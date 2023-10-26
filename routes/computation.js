e6b776c6d76738e52f0ed419f171fdbde88e6b27
Jaswitha-20 <s562626@nwmissouri.edu>
2023-10-25 23:22:14 -0500

    changes are added

--- routes/computation.js
+++ routes/computation.js
@@ -3,12 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  var random= Math.random();
   expon= Math.exp(random);
   res.send(`Exponential applied to ${random} is : ${expon}`);
 });
 
 module.exports = router;
 
-var random= Math.random();
+
 var expon;
 
